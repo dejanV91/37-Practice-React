eced81fad5c5a0a0f35c837ccc5a29fc3dfab112
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-02 14:17:25 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 const UseStateBasics = () => {
   const [text, setText] = useState("Random Value");
 
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
