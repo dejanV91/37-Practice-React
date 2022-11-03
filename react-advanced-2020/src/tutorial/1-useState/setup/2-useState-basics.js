4a50e3acdd80e02d9f64d67d6b93ed1a3c493170
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-03 14:21:28 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 
 const UseStateBasics = () => {
   const [text, setText] = useState("Random Value");
+  const handleChange = () => {};
 
   return (
     <React.Fragment>
       <h1>{text}</h1>
-      <button type="button" className="btn">
+      <button type="button" className="btn" onClick={handleChange}>
         Change Title
       </button>
     </React.Fragment>
