5b10b431cd359bd00bc67fe846b604ca9eb69a8b
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-04 14:24:30 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -5,6 +5,8 @@ const UseStateBasics = () => {
   const handleChange = () => {
     if ((text = "Random Value")) {
       setText("Hello World");
+    } else {
+      console.log("dejan");
     }
   };
 
