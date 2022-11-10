63ae80966faca804d10c3990b6754634970ca12a
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-10 21:04:33 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,13 +1,20 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { data } from "../../../data";
 
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState([data]);
+  const [people, setPeople] = React.useState(data);
 
   return (
-    <React.Fragment>
-      <h2>UseState Array</h2>
-    </React.Fragment>
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
