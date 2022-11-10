e2db70a8a06e9c133b90782ccf61016d32c591e3
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-10 21:31:28 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,7 +3,10 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
-
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <>
       {people.map((person) => {
@@ -11,9 +14,13 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear item
+      </button>
     </>
   );
 };
