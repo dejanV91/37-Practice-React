71a040e5d3acb0b63eb15505ab575fdff06ee4ab
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-11 16:10:18 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [counter, changeNumber] = useState(0);
+
+  const decrease = () => {
+    let i = counter - 1;
+    changeNumber(i);
+  };
+  const reset = () => {
+    changeNumber(0);
+  };
+  const increase = () => {
+    let i = counter + 1;
+    changeNumber(i);
+  };
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button className="btn" onClick={() => decrease()}>
+        decrease
+      </button>
+      <button className="btn" onClick={() => reset()}>
+        reset
+      </button>
+      <button className="btn" onClick={() => increase()}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
