b72d970745fc0c35a8f2850af2c5a50982f945e2
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-12 07:51:20 +0100

    initial commit

--- react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,18 +3,42 @@ import React, { useState } from "react";
 const UseStateCounter = () => {
   const [counter, changeNumber] = useState(0);
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      changeNumber((number) => {
+        return number + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <>
-      <h1>{counter}</h1>
-      <button className="btn" onClick={() => changeNumber(counter - 1)}>
-        decrease
-      </button>
-      <button className="btn" onClick={() => changeNumber(0)}>
-        reset
-      </button>
-      <button className="btn" onClick={() => changeNumber(counter + 1)}>
-        increase
-      </button>
+      <section>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={() => changeNumber(counter - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => changeNumber(0)}>
+          reset
+        </button>
+        <button className="btn" onClick={() => changeNumber(counter + 1)}>
+          increase
+        </button>
+      </section>
+      <section
+        style={{
+          border: "2px solid black",
+          padding: "50px 0",
+          width: "50%",
+          margin: "50px auto",
+        }}
+      >
+        <h2>More Complex Counter</h2>
+        <h1>{counter}</h1>
+        <button className="btn" onClick={() => complexIncrease()}>
+          increase
+        </button>
+      </section>
     </>
   );
 };
