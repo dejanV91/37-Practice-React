cc6c008192edf0716a8e1684782197254d98a29c
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-12 08:49:43 +0100

    initial commit

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("useEffect");
+    if (value > 0) {
+      document.title = `New Message (${value})`;
+    } else {
+      document.title = "React App";
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log("second effect");
+  }, []);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+      <button className="btn" onClick={() => setValue(value - 1)}>
+        decrease
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
