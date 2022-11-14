737174461f7b5a5afbe471ea1e4a6002ec352427
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-14 07:58:18 +0100

    intial commit

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
   const [isLoading, setLoading] = useState(false);
-  const [isError, setIsError] = useState(true);
+  const [isError, setIsError] = useState(false);
   const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299) {
+          return response.json();
+        } else {
+          setLoading(false);
+          setIsError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   if (isLoading) {
     return (
