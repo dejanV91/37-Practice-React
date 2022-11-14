4b3c4a70c466e509b6442cd172fc0eb5806adad1
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-14 08:20:39 +0100

    intial commit

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, steText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text && "Dejan"}</h1>
+    </>
+  );
 };
 
 export default ShortCircuit;
