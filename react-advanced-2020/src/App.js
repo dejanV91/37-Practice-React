bf1c798aa50b78669c8e4509e37367167286a291
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-25 13:11:07 +0100

    initial commit

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/6-useReducer/setup/index2";
 function App() {
   return (
     <div className="container">
--- react-advanced-2020/src/tutorial/6-useReducer/setup/index2.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index2.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Modal2 from "./Modal2";
 
 const index2 = () => {
+  const defaultState = {
+    people: [],
+    isModalOpen: true,
+    modalContent: "",
+  };
   const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <input
             type="text"
@@ -16,6 +26,12 @@ const index2 = () => {
         </div>
         <button type="submit">add</button>
       </form>
+      <div>
+        {state.people.map((item) => {
+          const { id, title } = item;
+          return <h2 key={id}>{title}</h2>;
+        })}
+      </div>
     </>
   );
 };
