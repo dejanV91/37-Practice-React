31ab9181d7a705651e1442109e999d870313a4b7
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-02 18:45:33 +0100

    initial commit

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaulImage from "../../../assets/default-image.jpeg";
+const Product = ({ image, name, price }) => {
+  return (
+    <article className="product">
+      <img src={image.url} alt={name} />
+      <h4>{name}</h4>
+      <p>{price}</p>
+    </article>
+  );
+};
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  name: "default name",
+  price: 3.99,
+  image: defaulImage,
 };
 
 export default Product;
